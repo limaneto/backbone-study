509283f7ed24392f9b82b8f214d4ea2c5d940f5b
Lima Neto <limanetocc@gmail.com>
2017-05-14 16:44:29 -0300

    :pencil: Add validation to title attribute.

--- app/model/movie-model.js
+++ app/model/movie-model.js
@@ -2,6 +2,10 @@ define([], function () {
   return Backbone.Model.extend({
     defaults: {
       coverImage: 'http://placehold.it/200x99'
+    },
+
+    validate: function (attr) {
+      if (typeof attr.title === 'undefined' || attr.title === '') { throw new Error('Movie needs title attribute.') }
     }
   })
 })

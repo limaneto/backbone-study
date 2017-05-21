c270bb94756800ef6abde368aee439edcf30b860
Lima Neto <limanetocc@gmail.com>
2017-05-21 19:46:23 -0300

    :fire: Delete useless files.

--- app/collection/collection.js
+++
@@ -1,9 +0,0 @@
-define(['movieModel', 'Backbone'],
-  function (Movie, Backbone) {
-    var Collection = Backbone.Collection.extend({
-      model: Movie,
-      url: 'http://localhost:3000/catalog'
-    })
-
-    return new Collection()
-  })
--- app/view/initialize.js
+++
@@ -1,7 +0,0 @@
-define(['app/router/app-router', 'Backbone'],
-function (Router, Backbone) {
-  var Initialize = Backbone.View.extend({})
-
-
-  return new Initialize()
-})
